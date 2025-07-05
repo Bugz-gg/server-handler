a75854d793dda4e9f1e97a3bc9a9ff6eea4484a2
Ilyes Bechoual <112644383+Bugz-gg@users.noreply.github.com>
2025-07-05 22:07:50 +0200

    Update commands.py

--- utils/commands.py
+++ utils/commands.py
@@ -16,7 +16,7 @@ def send_down():
 
 
 def send_start(save_name: str):
-    cmd = f"SAVE_NAME={save_name} NEW_SAVE=false docker compose up -d"
+    cmd = f"SAVE_NAME={save_name} docker compose up -d"
     return subprocess.run(cmd, shell=True, capture_output=True, text=True), cmd
 
 
